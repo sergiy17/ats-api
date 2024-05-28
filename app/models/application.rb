ad19f65d92ef5f2033bf52e023069ae0d4b552a6
Sergiy Parkhuts <sergiy.parkhuts@andopen.co>
2024-05-28 10:09:18 +0300

    create status for jon and application models

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,20 @@
 class Application < ApplicationRecord
   belongs_to :job
+  has_many :events, as: :aggregate, class_name: 'Application::Event'
+
+  def status
+    last_event = events.order(created_at: :desc).first
+    return 'applied' unless last_event
+
+    case last_event.type
+    when 'Application::Event::Interview'
+      'interview'
+    when 'Application::Event::Hired'
+      'hired'
+    when 'Application::Event::Rejected'
+      'rejected'
+    else
+      'applied'
+    end
+  end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,19 @@
 class Job < ApplicationRecord
   has_many :applications
+  has_many :events, as: :aggregate, class_name: 'Job::Event'
+  has_many :events, class_name: 'DomainEvent', as: :aggregate
+
+  def status
+    last_event = events.order(created_at: :desc).first
+    return 'deactivated' unless last_event
+
+    case last_event.type
+    when 'Job::Event::Activated'
+      'activated'
+    when 'Job::Event::Deactivated'
+      'deactivated'
+    else
+      'deactivated'
+    end
+  end
 end
