e0264cf0961b8b6a59f02b4854c3867aa8653339
Sergiy Parkhuts <sergiy.parkhuts@andopen.co>
2024-05-28 16:51:17 +0300

    fix issue with autoloading

--- app/models/application/event.rb
+++ app/models/application/event.rb
@@ -1,3 +1,8 @@
 class Application::Event < DomainEvent
   self.table_name = 'domain_events'
 end
+
+class Application::Event::Interview < Application::Event; end
+class Application::Event::Hired < Application::Event; end
+class Application::Event::Note < Application::Event; end
+class Application::Event::Rejected < Application::Event; end
